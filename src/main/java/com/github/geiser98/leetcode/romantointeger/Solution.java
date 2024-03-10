cd926c6b4478eade8206a181222fbe635ce6b56b
GeiseR98 <ipatovsergejj@yandex.ru>
2024-03-10 05:55:47 +0300

    feat: преобразование ветвлением

--- src/main/java/com/github/geiser98/leetcode/romantointeger/Solution.java
+++ src/main/java/com/github/geiser98/leetcode/romantointeger/Solution.java
@@ -3,5 +3,8 @@ package com.github.geiser98.leetcode.romantointeger;
 public class Solution {
     public static void main(String[] args) {
 
+        ConversionByBranching romanToInt = new ConversionByBranching();
+        System.out.println(romanToInt.romanToInt("III"));
+
     }
 }
